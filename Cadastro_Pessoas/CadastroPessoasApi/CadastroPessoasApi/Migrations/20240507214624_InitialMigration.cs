172712595b9b2cf5065464aeb7138b7a3020380c
unknown <pablito29.pablo@gmail.com>
2024-05-07 19:06:32 -0300

    Altera a conexão para usar o SQL de um container docker

---
+++ Cadastro_Pessoas/CadastroPessoasApi/CadastroPessoasApi/Migrations/20240507214624_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CadastroPessoasApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(255)", nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    Renda = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+            migrationBuilder.CreateIndex(
+               name: "IX_Pessoas_Cpf",
+               table: "Pessoas",
+               column: "Cpf",
+               unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
